36c3562854f827f981e4fadf2c7dc7e9623632d1
Lê Hữu Nghĩa <nghia.huu.le@viatech.com.vn>
2019-11-18 14:14:37 +0700

    Change titles and styling on homepage

--- README.md
+++
@@ -1,3 +0,0 @@
-# angular-5eoffm
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5eoffm)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  data: any;
+  selected: number;
+  constructor(private http: HttpClient) {
+    this.showConfig();
+  }
+
+  configUrl =
+    "https://sheets.googleapis.com/v4/spreadsheets/1u_GWrcWvMA1JyZn7c2-3DRizULz_U94BDYNnVezRp-A/values/A2:D15?key=AIzaSyBKIBofPF8VRrsvWmMqfjDGxTdEGIR7mLc";
+
+  getConfig() {
+    return this.http.get(this.configUrl);
+  }
+
+  showConfig() {
+    this.getConfig().subscribe(dataRes => {
+      this.data = dataRes;
+    });
+  }
+
+  luckyMe() {
+    this.selected = this.generateRandomInteger(0, this.data.values.length);
+  }
+  generateRandomInteger(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  }
 }
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
