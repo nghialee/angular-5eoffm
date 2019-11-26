cb77e5a7b6114d841057e0257cd548132cbbe6e0
nghialee <nghiale.31092@gmail.com>
2019-11-26 14:16:39 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HttpClient } from "@angular/common/http";
 })
 export class AppComponent {
   data: any;
-  mode: string = "Expensive";
+  mode: string = undefined;
 
   selected: number;
   isFinish: boolean = true;
